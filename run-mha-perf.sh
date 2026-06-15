a7c181e2a7807a3cc3eb85e24e88ab3e30c0da42
jli10004 <Jun.Li@amd.com>
2026-06-15 04:22:24 +0000

    add perf shell and random-value parameter to reproduce the clock frequence issue

---
+++ run-mha-perf.sh
@@ -0,0 +1,2 @@
+python op_tests/test_mha_flydsl_varlen.py -b 1 -nh 64 -sq 16384 -sk 16384 --random-value false --warmup 5 --repeat 20
+python op_tests/test_mha_flydsl_varlen.py -b 1 -nh 64 -sq 16384 -sk 16384 --random-value true --warmup 5 --repeat 20
